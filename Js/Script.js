72c239be4194c301c960f7b0785d325799b60541
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2023-11-15 14:41:28 +0530

    Objects in array.
    Second task .

--- Js/Script.js
+++ Js/Script.js
@@ -1,5 +1,33 @@
-// alert pop up
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood) ; 
-
+//array is pkemonlist, objects are the pokemon within:
+let pokemonlist = 
+[
+    {//object1
+    name: "Lugia",
+    type: ["psychic", "flying"],
+    weight: "211",  //kg
+    height: "17",  //feet
+    rarity: "legendary",
+    },
+    {//object2
+        name: "Ho-oh",
+        type: ["fire","flying"],
+        weight: "200",  //kg    
+        height: "12",   //feet
+        rarity: "legendary",
+    },
+    {//object3
+        name: "Celebi",
+        type: ["psychic","grass"],
+        weight: "5",    //kg
+        height: "2",    //feet
+        rarity: "legendary",
+    },
+    {//object3
+        name:"pikachu",
+        type:["electric"],
+        weight:"1", //kg
+        height:"6", //feet
+        rarity:"normal",
+    }
+   
+]
