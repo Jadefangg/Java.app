0b7cf1662eabf5f596c451c332742988daea2046
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-01-09 14:19:43 +0530

    1.10
    1.10 modal works but not in cards

--- Js/Script.js
+++ Js/Script.js
@@ -4,28 +4,23 @@ const openModal=document.querySelector('.open-button')
 const closeModal = document.querySelector(".close-button");
 
 const fetchPokemon = () => {    //empty array
-
     const promises = [];
     for (let i = 1; i <= 150; i++) {
-
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
     Promise.all(promises).then((results) => {
-
         const pokemon = results.map((result) => ({
-
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
+             name: result.name,
+             image: result.sprites['front_default'],
+             type: result.types.map((type) => type.type.name).join(', '),
+             id: result.id
         }));
         displayPokemon(pokemon);
     });
 };
 
 const displayPokemon = (pokemon) => {
-
     console.log(pokemon);
     const pokemonHTMLString = pokemon
         .map(
@@ -35,14 +30,25 @@ const displayPokemon = (pokemon) => {
             <img class="card-image" src="${pokeman.image}"/>
             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
             <p class="card-subtitle">Type: ${pokeman.type}</p>
-                openModal.addEventlistener('click',() =>{
-                modal.showModal();
-        
+            <button class="button open-button">open modal</button>
+        <dialog class="modal" id="modal">
+            <h2>An interesting title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum esse nisi, laboriosam illum temporibus ipsam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quo.</p>
+            <button class="button close-button">close modal</button>
+        </dialog>
+       
         </li> `
         )
+        
         .join('');
+        openModal.addEventListener("click", () => {
+            modal.showModal();
+          });
+          closeModal.addEventListener("click", () => {
+            modal.close();
+          });
+          
         pokedex.innerHTML = pokemonHTMLString;
 }
-   
-
-fetchPokemon();
+   fetchPokemon();
